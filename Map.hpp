3c9154717d63a5f5dbb63d6e5decd0b0cb554558
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-07-12 17:27:11 +0200

    Add files via upload

--- Map.hpp
+++ Map.hpp
@@ -1,9 +1,23 @@
 #pragma once
 #include <raylib.h>
+#include <cstdint>
+
 
 class Map {
+private:
+	typedef struct Block {
+		Texture2D Texture;
+		Rectangle Collision;
+		Vector2 Position;
+	} Block;
+
+	Block *_Block;
+
 public:
-	Map();
+	Map(uint16_t SizeOfMap, int32_t seed);
 	~Map();
-	
+
+	void Draw();
+	Rectangle GetBlockColission();
+
 };
